53f30f434ff5aa55182470bdb17b4e38446c5154
Alexey Ryabinkin <a.ryabinkin71@gmail.com>
2025-07-19 17:51:29 +0300

    lesson_06

---
+++ lesson_6/Task_01/Neto_06_01.cpp
@@ -0,0 +1,41 @@
+﻿// Neto_06_01.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+
+
+template<class T>
+void printC(const T& X, const std::string* str)
+{
+    std::cout << "[IN]: " << *str << std::endl;
+    std::cout << "[OUT]: " << std::endl;
+    for (const auto& x : X)
+    {
+        std::cout << x.second << ":  " << x.first << std::endl;
+    }
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    size_t num = 0;
+    std::string str = "Hello World!!!";
+    std::multimap<size_t, char, std::greater<size_t>> xx;//вывод контейнера с сортировкой по убыванию
+
+    std::set<char> S;//создаем ассоциативный контейнер
+
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        S.insert(str[i]);//заполняем контейнер строковыми данными
+    }
+    for (const auto& x : S)//поддержка range based for
+    {
+        num = std::count(str.begin(), str.end(), x);//подсчет одинаковых символов в указанном диапазоне
+        xx.emplace(num, x);//вставка результатов счета
+    }
+    printC(xx, &str);//вывод результатов подсчета количества одинаковых символов
+
+    return 0;
+}
---
+++ lesson_6/Task_02/Neto_06_02.cpp
@@ -0,0 +1,26 @@
+﻿// Neto_06_02.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <set>
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    int number = 0;
+    std::cout << "[IN]: " << std::endl;
+    std::cin >> number;//ввод количества элементов
+    std::set<int, std::greater<int>> val;//создание контейнера
+    for (size_t i{ 0 }, v{ 0 }; i < number; i++)
+    {
+        //заполнение контейнера
+        std::cin >> v;
+        val.insert(v);
+    }
+    std::cout << "[OUT]:" << std::endl;
+    for (const auto& v : val)
+    {
+        std::cout << v << std::endl;//вывод элементов без повторов и в порядке убывания
+    }
+    return 0;
+}
