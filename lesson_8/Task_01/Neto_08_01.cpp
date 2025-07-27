c1218d3574afac7d16bc6dbb10f4c4f1716a4efe
Alexey Ryabinkin <a.ryabinkin71@gmail.com>
2025-07-27 20:04:47 +0300

    lesson_8

---
+++ lesson_8/Task_01/Neto_08_01.cpp
@@ -0,0 +1,39 @@
+﻿// Neto_08_01.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename T> //шаблонная функция для удаления дубликатов
+void remove_duplicates(std::vector<T>& v)
+{
+    std::sort(v.begin(), v.end()); //сначала сортируем вектор
+    auto it = std::unique(v.begin(), v.end()); //удаление дубликатов вектора
+    v.erase(it, v.end()); //стирание ненужных элементов (дубликатов)
+}
+
+template <class T> //шаблонный класс для вывода вектора в консоль
+void print_vector(std::vector<T>& v)
+{
+    std::vector<int>::iterator itr = v.begin(); //используем итератор
+    while (itr != v.end())
+    {
+        std::cout << *itr << " ";
+        itr++;
+    }
+}
+int main()
+{
+    std::vector<int> v{ 1,1,2,3,1,4,5,5,7,6 };
+    std::cout << "[IN]: ";
+    print_vector(v); //вызов функции для вывода исходного вектора в консоль
+    std::cout << std::endl;
+    
+    remove_duplicates(v); //вызов функции для удаления дубликатов 
+    std::cout << "[OUT]: ";
+
+    print_vector(v); //вызов функции для вывода отформатированного вектора в консоль
+    return 0;
+}
+
---
+++ lesson_8/Task_02/Neto_08_02.cpp
@@ -0,0 +1,38 @@
+﻿// Neto_08_02.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+
+template <typename T> //шаблонная функция для печати содержимого контейнера
+void print_container(const T& container)
+{
+    for (auto& elem : container)
+    {
+        std::cout << elem << " ";
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    std::set<std::string> test_set{ "one", "two", "three", "four" };
+    std::cout << "Контейнер [test_set]: ";
+    print_container(test_set); //вызов функции для печати контейнера
+    std::cout << std::endl;
+
+    std::list<std::string> test_list{ "one", "two", "three", "four" };
+    std::cout << "Контейнер [test_list]: ";
+    print_container(test_list); //вызов функции для печати контейнера
+    std::cout << std::endl;
+
+    std::vector<std::string> test_vector{ "one", "two", "three", "four" };
+    std::cout << "Контейнер [test_vector]: ";
+    print_container(test_vector); //вызов функции для печати контейнера
+    std::cout << std::endl;
+
+    return 0;
+}
